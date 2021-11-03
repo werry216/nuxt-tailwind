9e78075d1e3098c02b85b228737b29c84e54e830
sunshine1004-dev <grand11sea@gmail.com>
2021-11-03 10:20:28 +0700

    complete first section

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="grid grid-cols-3 gap-2  0 m-10">
-    <index1-vue></index1-vue>
-    <index2-vue></index2-vue>
-    <index3-vue></index3-vue>
+  <div class="bg-gray-100 font-sans w-full min-h-screen m-0">
+    <Header></Header>
+    <div class="w-full rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+        <div class="text-center">
+            <p class="mb-3 text-6xl">Deine Job website</p>
+        </div>
+        <div class="mb-8">
+            <img class="object-center object-cover rounded-full h-418 w-405" src="/images/undraw_agreement_aajr.png" alt="Undraw agreement">
+        </div>
+    </div>
+    <div class="bg-white shadow border-t-4 h-32 rounded-t-lg">
+        <div class="container mx-auto px-4">
+            <div class="block sm:hidden bg-white py-2">
+                <div class="flex flex-col">
+                    <div class="flex items-center pt-2 bg-gradient-to-r from-green-400 to-blue-500 rounded-full py-3 px-6 justify-center text-sm font-semibold text-white">
+                        Kostenlos Registrieren
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Index1Vue from "~/components/index1";
-import Index2Vue from "~/components/index2";
-import Index3Vue from "~/components/index3";
+import Header from "~/components/Header";
 export default {
   components: {
-    Index1Vue,
-    Index2Vue,
-    Index3Vue
+    Header,
   }
 }
 </script>
