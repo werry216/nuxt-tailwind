c123b283956ed379f64287fd68a9e1d22df7b23e
werry216 <87049471+werry216@users.noreply.github.com>
2021-12-08 03:20:45 +0200

    Fixed footer

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,8 +1,12 @@
 <template>
-    // this is the main content...
     <div class="bg-white shadow border-t-4 h-24 rounded-t-2xl fixed w-full bottom-0 flex flex-col justify-center">
       <div class="container mx-auto px-4">
         <div class="block bg-white py-2">
+          <div class="flex flex-col">
+            <div
+              class="flex items-center pt-2 bg-gradient-to-r from-green-400 to-blue-500 rounded-xl py-3 px-6 justify-center text-sm font-semibold text-white"
+            >Kostenlos Registrieren</div>
+          </div>
           <div class="flex flex-col">
             <div
               class="flex items-center pt-2 bg-gradient-to-r from-green-400 to-blue-500 rounded-xl py-3 px-6 justify-center text-sm font-semibold text-white"
