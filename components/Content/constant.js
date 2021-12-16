a529f4881d7787a71d9de29ef73e0ea2e1060882
werry216 <87049471+werry216@users.noreply.github.com>
2021-12-16 04:11:22 +0200

    fixed constant of content

--- components/Content/constant.js
+++ components/Content/constant.js
@@ -1,5 +1,12 @@
 export default main_constant = {
   index: 1,
   update: 2,
-  pollo: 3
+  pollo: 3,
+  new: 4
+}
+
+export const updateObject = {
+  salary: 1000,
+  fee: 200,
+  implement: 300
 }
